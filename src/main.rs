d35622aa11b97a453c19407d10231a8747281551
isalevine <isa.loves.coding@gmail.com>
2019-06-18 23:37:00 -0700

    reorganized struct/functions, added main turn loop

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,8 @@
 use std::io; 
+use std::process;
 
 
 fn main() {
-
-    struct Character {
-        name: String,
-        hp: i32,
-        attack: String,
-        damage: i32,
-    };
-
-    impl Character {
-        fn print_character_hp(&self) {
-            println!("{}'s HP is currently {}.", self.name, self.hp);
-            if self.hp <= 0 {
-                println!("{} is dead!", self.name)
-            };
-        }
-
-        fn print_character_attack(&self) {
-            println!("{}'s attack is {}, and it does {} damage.", self.name, self.attack, self.damage);
-        }
-
-        fn take_damage(&mut self, character: &Character) {
-            println!("{} takes {} damage from {}!", self.name, character.damage, character.name);
-            if self.hp - character.damage < 0 {
-                self.hp = 0
-            } else {
-                self.hp -= character.damage
-            }
-        }
-    }
 
     let mut knight = Character {
         name: String::from("Knight"),
@@ -46,19 +18,8 @@ fn main() {
         damage: 3,
     };
 
-    knight.print_character_hp();
-    lizardman.print_character_attack();
-    knight.take_damage(&lizardman);
-    knight.print_character_hp();
 
-    lizardman.print_character_hp();
-    lizardman.print_character_attack();
-    lizardman.take_damage(&knight);
-    lizardman.print_character_hp();
-    lizardman.take_damage(&knight);
-    lizardman.print_character_hp();
-
-    player_menu();
+    main_turn_loop(knight, lizardman);
 
     // next functions:
     // if-statement for hp 0 or below => dead
@@ -68,8 +29,61 @@ fn main() {
 }
 
 
-pub fn player_menu() {
-    println!("Please select an option:
+
+pub struct Character {
+    name: String,
+    hp: i32,
+    attack: String,
+    damage: i32,
+}
+
+impl Character {
+    fn print_character_hp(&self) {
+        println!("{}'s HP is currently {}.", self.name, self.hp);
+        if self.hp <= 0 {
+            println!("{} is dead!", self.name);
+
+            // temp
+            println!("You win!! (Exiting program now.)");
+            std::process::exit(0);
+        };
+    }
+
+    fn print_character_attack(&self) {
+        println!("{}'s attack is {}, and it does {} damage.", self.name, self.attack, self.damage);
+    }
+
+    fn take_damage(&mut self, character: &Character) {
+        println!("{} takes {} damage from {}!", self.name, character.damage, character.name);
+        if self.hp - character.damage < 0 {
+            self.hp = 0
+        } else {
+            self.hp -= character.damage
+        }
+    }
+}
+
+
+
+
+pub fn main_turn_loop(mut player: Character, mut enemy: Character) {
+    player.print_character_hp();
+    // enemy.print_character_attack();
+    // player.take_damage(&enemy);
+    // player.print_character_hp();
+
+    enemy.print_character_hp();
+    // enemy.print_character_attack();
+    // enemy.take_damage(&player);
+    // enemy.print_character_hp();
+    // enemy.take_damage(&player);
+    // enemy.print_character_hp();
+
+    player_menu(player, enemy);
+}
+
+pub fn player_menu(mut player: Character, mut enemy: Character) {
+    println!("Your turn! Please select an option:
     1) Attack
     2) Defend
     3) Retreat
@@ -79,16 +93,20 @@ pub fn player_menu() {
 
     if input == "1" || input == "attack" || input == "Attack" || input == "a" {
         println!("Player attacks!");
+        enemy.take_damage(&player);
+        enemy.print_character_hp();
     } else if input == "2" || input == "defend" || input == "Defend" || input == "d" {
         println!("Player defends!");
     } else if input == "3" || input == "retreat" || input == "Retreat" || input == "r" {
         println!("Player retreats!");
     } else if input == "0" || input == "quit" || input == "Quit" || input == "q" {
-        println!("Player defends!");
+        println!("Exiting program. Goodbye!");
+        std::process::exit(0);
     } else {
         println!("Invalid input!");
-        player_menu();
     };
+
+    player_menu(player, enemy);
 
     // println!("input is: {}", input)
 }
